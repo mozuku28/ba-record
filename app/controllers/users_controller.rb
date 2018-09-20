f21d7b418450bb4a77fec0e64f5f110b644311a1
mozuku28 <mozuku28@gmail.com>
2018-09-20 10:31:04 +0000

    管理者の権限設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_admin! || :authenticate_user!, only: [:index, :edit, :update, :destroy]
 
   layout 'users'
 
@@ -10,6 +11,17 @@ class UsersController < ApplicationController
 
   def edit
     @users = User.find(params[:id])
+    unless admin_signed_in?
+      if @users.id != current_user
+        redirect_to new_user_session_path
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path(@user)
   end
 
   def search
@@ -17,5 +29,8 @@ class UsersController < ApplicationController
     redirect_to admins_users_path
   end
 
-
+  private
+      def user_params
+          params.require(:user).permit(:name_family_name, :name_name, :furigana_family_name, :furigana_name, :postal_code, :address, :phone_number, :email)
+      end
 end
