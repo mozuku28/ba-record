63d76b8014546bfec28d338e151ec3a21ef5e204
mozukun28 <mozukun28@gmail.com>
2018-09-21 10:00:21 +0000

    テストコミット

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,8 @@ class ProductsController < ApplicationController
       @product = Product.new
   end
 
-
+  # private
+  # def product_params
+  #     params.require(:product).permit(:title, :label_id, :image, :release_date, :stock_count)
+  # end
 end
